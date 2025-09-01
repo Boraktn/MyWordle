a4f600f1bf855577e9eafaf942da21af516be079
boraktn <brktn1212@gmail.com>
2025-09-01 11:38:06 +0300

    güncelleme

--- control.js
+++ control.js
@@ -2,6 +2,29 @@ const myDiv= document.getElementById("myDiv");
 const answer= ["C","E","V","A","P"];
 const inputs = document.querySelectorAll(".ans");
 
+
+function focusFirstEmpty() {
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === "") {
+      inputs[i].focus();
+      return;
+    }
+  }
+}
+function refresh(){
+  const userInput = Array.from(inputs).map(inp => inp.value.toUpperCase());
+      const children = myDiv.children;
+  for(let i=0; i<5; i++){
+    inputs[i].value = "";
+    children[i].style.backgroundColor  = "#F3F3E0"
+  }
+  focusFirstEmpty();
+}
+window.addEventListener("DOMContentLoaded", () => {
+  focusFirstEmpty();
+});
+
+
 inputs.forEach((input, index) => {
   input.addEventListener("input", () => {
     if (input.value.length === 1 && index < inputs.length - 1) {
@@ -16,14 +39,11 @@ inputs.forEach((input, index) => {
         inputs[index - 1].value = "";
       }
     }
-    if (event.key === "Enter" && i) {
+    if (event.key === "Enter" && inputs[4].value !== "") {
     const userInput = Array.from(inputs).map(inp => inp.value.toUpperCase());
       const children = myDiv.children;
     for(let step=0; step <5; step++){
-        console.log("nicce");
         if(userInput[step]==answer[step]){
-                    console.log("4");
-
             children[step].style.backgroundColor = "green";
             }
         else{
@@ -32,6 +52,9 @@ inputs.forEach((input, index) => {
         }
         }
     }
+  });
+  input.addEventListener("blur", () => {
+    setTimeout(focusFirstEmpty, 0);
   });
 });
   
