dfd87524b1b5066d056e504923f882fea4f00c3e
boraktn <brktn1212@gmail.com>
2025-09-03 13:15:45 +0300

    güncelleme

--- control.js
+++ control.js
@@ -1,21 +1,28 @@
 const myDiv= document.getElementById("myDiv");
 const answer= ["C","E","V","A","P"];
-const inputs = document.querySelectorAll(".ans");
+const inputs = document.querySelectorAll("#div1 .ans");
+const inputs2 = document.querySelectorAll("#div2 .ans");
+const inputs3 = document.querySelectorAll("#div3 .ans");
+const inputs4 = document.querySelectorAll("#div4 .ans");
+const inputs5 = document.querySelectorAll("#div5 .ans");
+const inputs6 = document.querySelectorAll("#div6 .ans");
+const inputList = [inputs, inputs2, inputs3, inputs4, inputs5, inputs6];
+let row = 0;
 
 
 function focusFirstEmpty() {
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value === "") {
-      inputs[i].focus();
+  for (let i = 0; i < inputList[row].length; i++) {
+    if (inputList[row][i].value === "") {
+      inputList[row][i].focus();
       return;
     }
   }
 }
 function refresh(){
-  const userInput = Array.from(inputs).map(inp => inp.value.toUpperCase());
+  const userInput = Array.from(inputList[row]).map(inp => inp.value.toUpperCase());
       const children = myDiv.children;
   for(let i=0; i<5; i++){
-    inputs[i].value = "";
+    inputList[row][i].value = "";
     children[i].style.backgroundColor  = "#F3F3E0"
   }
   focusFirstEmpty();
@@ -25,23 +32,24 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 
-inputs.forEach((input, index) => {
+inputList[row].forEach((input, index) => {
   input.addEventListener("input", () => {
-    if (input.value.length === 1 && index < inputs.length - 1) {
-      inputs[index + 1].focus();
+    if (input.value.length === 1 && index < inputList[row].length - 1) {
+      inputList[row][index + 1].focus();
+    console.log("b");  
     }
   });
 
   input.addEventListener("keydown", (event) => {
     if (event.key === "Backspace" && input.value === "") {
       if (index > 0) {
-        inputs[index - 1].focus();
-        inputs[index - 1].value = "";
+        inputList[row][index - 1].focus();
+        inputList[row][index - 1].value = "";
       }
     }
-    if (event.key === "Enter" && inputs[4].value !== "") {
-    const userInput = Array.from(inputs).map(inp => inp.value.toUpperCase());
-      const children = myDiv.children;
+    if (event.key === "Enter" && inputList[row][4].value !== "") {
+    const userInput = Array.from(inputList[row]).map(inp => inp.value.toUpperCase());
+      const children = myDiv.children[row].children;
     for(let step=0; step <5; step++){
         if(userInput[step]==answer[step]){
             children[step].style.backgroundColor = "green";
@@ -51,6 +59,8 @@ inputs.forEach((input, index) => {
 
         }
         }
+        row = row  + 1;
+        focusFirstEmpty();
     }
   });
   input.addEventListener("blur", () => {
